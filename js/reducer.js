02e621cef3de2ce54e900f310677902fea81a241
jdhofmann <justindhofmann@gmail.com>
2020-11-14 09:16:51 -0500

    ya

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,29 @@
 // add code snippets from README
+let state
+
+const reducer = (state  = {count: 0}, action) => {
+    switch (action.type){
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1}
+        default :
+            return state;
+    }
+}
+
+let button = document.querySelector('#button');
+
+button.addEventListener('click', ()=> {
+    dispatch({type: "INCREASE_COUNT"})
+})
+
+const render = () => {
+    let container = document.querySelector("#container");
+    container.textContent = state.count;
+}
+
+const dispatch = (action) => {
+    state = reducer(state, action)
+    render()
+}
+
+dispatch({type: '@@INIT'})
